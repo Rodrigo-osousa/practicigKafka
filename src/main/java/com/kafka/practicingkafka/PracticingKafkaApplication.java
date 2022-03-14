39cee45019b5b4971ab15581e874fa09b76e67c1
Rodrigo O.Sousa <rodrigo_oliveira_sousa@hotmail.com>
2022-03-14 20:06:47 -0300

    new method off producer add

--- src/main/java/com/kafka/practicingkafka/PracticingKafkaApplication.java
+++ src/main/java/com/kafka/practicingkafka/PracticingKafkaApplication.java
@@ -1,22 +1,34 @@
 package com.kafka.practicingkafka;
 
+import com.kafka.practicingkafka.anotherForm.ProducerEvent;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 
+
 @SpringBootApplication
 public class PracticingKafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticingKafkaApplication.class, args);
+
+
+		PracticingKafkaApplication application = new PracticingKafkaApplication();
+		application.kafkaRun();
 	}
+		private void kafkaRun() {
+			ProducerEvent producer = new ProducerEvent();
+			producer.runKafka();
+		}
+
 
 	@Bean
 	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
 		return args -> {
-			kafkaTemplate.send("testtopic","Hello Kafka!");
+			for(int i = 0; i < 10000; i++) {
+			kafkaTemplate.send("testtopic","Hello Kafka!" + i);}
 		};
 	}
 
